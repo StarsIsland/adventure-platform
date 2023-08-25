5fd5bfae507f1abdda3a9ce659aa71bd4c833bfd
Peng_Lx <L834787411@outlook.com>
2023-08-25 14:33:42 +0800

    Update StarkkitAudience.java
    Update StarkkitAudience.java

--- platform-starkkit/src/main/java/net/kyori/adventure/platform/starkkit/StarkkitAudience.java
+++ platform-starkkit/src/main/java/net/kyori/adventure/platform/starkkit/StarkkitAudience.java
@@ -30,7 +30,6 @@ import net.kyori.adventure.platform.facet.Facet;
 import net.kyori.adventure.platform.facet.FacetAudience;
 import net.kyori.adventure.platform.facet.FacetAudienceProvider;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 final class StarkkitAudience extends FacetAudience<CommandSender> {
 
@@ -39,11 +38,8 @@ final class StarkkitAudience extends FacetAudience<CommandSender> {
           StarkkitFacet.ChatConsole::new);
   private static final Collection<Facet.ActionBar<Player, ?>> ACTION_BAR = Facet.of(
           StarkkitFacet.ActionBar::new);
-  private static final Collection<Facet.Title<Player, ?, ?, ?>> TITLE = Facet.of();
-  private static final Collection<Facet.BossBar.Builder<Player, ? extends Facet.BossBar<Player>>> BOSS_BAR = Facet.of();
-  private static final Collection<Facet.Pointers<? extends CommandSender>> POINTERS = Facet.of();
 
-  StarkkitAudience(@NotNull FacetAudienceProvider provider, @NotNull Collection<? extends CommandSender> viewers, @Nullable Collection<? extends Facet.Chat> chat, @Nullable Collection<? extends Facet.ActionBar> actionBar, @Nullable Collection<? extends Facet.Title> title, @Nullable Collection<? extends Facet.Sound> sound, @Nullable Collection<? extends Facet.EntitySound> entitySound, @Nullable Collection<? extends Facet.Book> book, @Nullable Collection<? extends Facet.BossBar.Builder> bossBar, @Nullable Collection<? extends Facet.TabList> tabList, @Nullable Collection<? extends Facet.Pointers> pointerProviders) {
-    super(provider, viewers, chat, actionBar, title, sound, entitySound, book, bossBar, tabList, pointerProviders);
+  StarkkitAudience(@NotNull FacetAudienceProvider provider, @NotNull Collection<? extends CommandSender> viewers) {
+    super(provider, viewers, CHAT, ACTION_BAR , null, null, null, null, null, null, null);
   }
 }
